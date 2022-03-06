7be337f3111d491b6bd05601155479a2ad172682
Kevin Day <kevin.ch.day@gmail.com>
2022-03-06 11:52:59 -0600

    Initial Commit

---
+++ workspace_1/PHP_App_1.php
@@ -0,0 +1,19 @@
+<?php
+/* PHP_App_1.php
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Page</title>
+</head>
+
+<body>
+<?php
+echo "<p>Hello World</p>";
+?>
+</body>
+</html> 
